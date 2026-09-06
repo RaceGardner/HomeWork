6efc9aef3863d9bc0c328248dab9c33d751d8373
RaceGardner <ragardner@csumb.edu>
2026-09-06 10:55:10 -0700

    created Learning and Learning test classes for UML hw, pushing to generate UML

--- src/main/java/Learning.java
+++ src/main/java/Learning.java
@@ -1,4 +1,72 @@
+/**
+ * description
+ * @author Race Gardner
+ * @version 0.1.0
+ * @since 09/06/2026
+ */
 public class Learning {
+    public static final int VERSION = 42;
+
+    private int x;
+    private int y;
+
+    protected String operation;
+
+    double output;
+
+    public Learning(int x, int y, String operation) {
+        this.x = x;
+        this.y = y;
+        this.operation = operation;
+    }
+
+    /**
+     * Performs set operations on parameters x and y
+     * @param x an integer
+     * @param y an integer
+     * @return output a double
+     */
+    public double operate(int x, int y){
+        switch(operation){
+            case "addition":
+            case "add":
+                output = x+y;
+                System.out.printf("%d + %d == %f",x,y,output);
+                break;
+            case "subtraction":
+                output = x-y;
+                System.out.printf("%d + %d == %f",x,y,output);
+                break;
+            default:
+                output = 0;
+                System.out.printf("I don't know what %s is %n", operation);
+        }
+        return output;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
 
 
 }
---
+++ src/test/java/LearningTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LearningTest {
+
+    Learning addition;
+    Learning subtraction;
+    int x = 6;
+    int y = 7;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        addition = new Learning(x,y,"add");
+        subtraction = new Learning(x,y,"subtraction");
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+        addition = null;
+        subtraction = null;
+    }
+
+    @org.junit.jupiter.api.Test
+    void operate() {
+        assertEquals(x+y, addition.operate(x,y));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getX() {
+        assertEquals(x, addition.getX());
+        assertEquals(x, subtraction.getX());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setX() {
+        assertEquals(x,addition.getX());
+        addition.setX(10);
+        assertNotEquals(x,addition.getX());
+        assertEquals(10,addition.getX());
+        assertNotEquals(addition.getX(),subtraction.getX());
+    }
+}
